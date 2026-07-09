225874028aa552365fbfbd062c17e2b7481b92c1
Ben A L Jemmett <ben@jemmett.me.uk>
2026-07-09 18:01:06 +0000

    Add Piper support to SpeechSynthesizer.pm.
    This is for local testing, primarily, as the results do not sound as good as
    the existing AWS Polly support; however, Piper has the distinct advantage of
    being free!  It is enabled by default; to run using Polly, set
    $SpeechSynthesizer::mode to 'polly' before calling generate().

--- lib/SpeechSynthesizer.pm
+++ lib/SpeechSynthesizer.pm
@@ -3,14 +3,15 @@ package SpeechSynthesizer;
 use strict;
 use warnings;
 
-use strict;
-use warnings;
+use LWP::UserAgent;
+use JSON;
 
 use Paws;
 
-die "AWS credentials required in environment" unless $ENV{AWS_ACCESS_KEY} && $ENV{AWS_SECRET_KEY};
+my $mode = "piper";
 
-my $polly = Paws->service('Polly', region => 'eu-west-2');
+my $polly;
+my $ua = LWP::UserAgent->new;
 
 my %ssml_entities = (
     "'" => "&apos;",
@@ -21,6 +22,7 @@ my %ssml_entities = (
 
 my $newsreader = 1;
 my $mp3_dir = "sound-files/mp3";
+my $wav_dir = "sound-files/wav";
 my $pcm_dir = "sound-files/pcm";
 
 sub make_newsreader_ssml($)
@@ -37,7 +39,7 @@ sub make_newsreader_ssml($)
     '<speak><amazon:domain name="news">' . $text . '</amazon:domain></speak>';
 }
 
-sub generate_mp3($$)
+sub generate_polly_mp3($$)
 {
     my $text = shift;
     my $filename = shift;
@@ -53,6 +55,12 @@ sub generate_mp3($$)
     {
         $text = make_newsreader_ssml($text);
         $type = 'ssml';
+    }
+
+    unless (defined $polly)
+    {
+        die "AWS credentials required in environment" unless $ENV{AWS_ACCESS_KEY} && $ENV{AWS_SECRET_KEY};
+        $polly = Paws->service('Polly', region => 'eu-west-2');
     }
 
     my $res = $polly->SynthesizeSpeech(
@@ -72,6 +80,31 @@ sub generate_mp3($$)
     1;
 }
 
+sub generate_piper_wav($$)
+{
+    my $text = shift;
+    my $filename = shift;
+
+    if (-f $filename)
+    {
+        print "$filename already exists, not re-generating.\n";
+        return 1;
+    }
+
+    my $blob = {
+        "text" => $text
+    };
+
+    my $req = HTTP::Request->new(POST => 'http://localhost:5001/');
+    $req->content_type('application/json');
+    $req->content(encode_json($blob));
+
+    my $res = $ua->request($req, $filename);
+    die $res->status_line unless $res->is_success;
+
+    1;
+}
+
 sub convert_to_pcm($$)
 {
     my $mp3 = shift;
@@ -83,10 +116,26 @@ sub convert_to_pcm($$)
         return 1;
     }
 
-    my $command = "ffmpeg -hide_banner -loglevel error -stats -i \"$mp3\" -f -lavfi -f mulaw \"$pcm\"";
+    my $command = "ffmpeg -hide_banner -loglevel error -stats -i \"$mp3\" -ar 8000 -f -lavfi -f mulaw \"$pcm\"";
 
     die "Cannot spawn '$command': $!" if system($command) == -1;
     die "Potential ffmpeg error $?" if $?;
+}
+
+sub generate_polly($$)
+{
+    my $text = shift;
+    my $filename = shift;
+
+    generate_polly_mp3($text, "$mp3_dir/$filename.mp3") && convert_to_pcm("$mp3_dir/$filename.mp3", "$pcm_dir/$filename.ulaw");
+}
+
+sub generate_piper($$)
+{
+    my $text = shift;
+    my $filename = shift;
+
+    generate_piper_wav($text, "$wav_dir/$filename.wav") && convert_to_pcm("$wav_dir/$filename.wav", "$pcm_dir/$filename.ulaw");
 }
 
 sub generate($$)
@@ -94,7 +143,14 @@ sub generate($$)
     my $text = shift;
     my $filename = shift;
 
-    generate_mp3($text, "$mp3_dir/$filename.mp3") && convert_to_pcm("$mp3_dir/$filename.mp3", "$pcm_dir/$filename.ulaw");
+    if ($mode eq "piper")
+    {
+        generate_piper($text, $filename);
+    }
+    else
+    {
+        generate_polly($text, $filename);
+    }
 }
 
 sub create_directories($;$)
@@ -104,22 +160,24 @@ sub create_directories($;$)
 
     mkdir $basedir or die "Cannot create directory $basedir: $!" unless -d $basedir;
 
-    for (qw(mp3 pcm))
+    for (qw(mp3 wav pcm))
     {
         mkdir "$basedir/$_" or die "Cannot create directory $basedir/$_: $!" unless -d "$basedir/$_";
     }
 
     $mp3_dir = "$basedir/mp3";
+    $wav_dir = "$basedir/wav";
     $pcm_dir = "$basedir/pcm";
 
     if ($subdir ne ".")
     {
-        for (qw(mp3 pcm))
+        for (qw(mp3 wav pcm))
         {
             mkdir "$basedir/$_/$subdir" or die "Cannot create directory $basedir/$_/$subdir: $!" unless -d "$basedir/$_/$subdir";
         }
 
         $mp3_dir .= "/$subdir";
+        $wav_dir .= "/$subdir";
         $pcm_dir .= "/$subdir";
     }
 }
